2ec4887f0c7334224416d64a238c8d6f8d851640
doh <theshawns0n@gmail.com>
2020-11-17 11:37:36 -0500

    no errors

--- client/src/util/API.js
+++ client/src/util/API.js
@@ -3,37 +3,41 @@ import axios from "axios";
 
 
 // findCoins: function() {
-//     return axios.get("https://api.coingecko.com/api/v3/coins/list");
+//     axios.get("https://api.coingecko.com/api/v3/coins/list");
 //   },
 
 // DB calls
 
 // Gets all coins
-getCoins: function() {
-    return axios.get("/api/coins");
-},
-// Gets the book with the given id
-getCoin: function(id) {
-    return axios.get("/api/coins/" + id);
-},
+const getCoins = () =>
+    axios.get("/api/coins");
+
+// Gets the coin with the given id
+const getCoin = (id) =>
+    axios.get("/api/coins/" + id);
+
 // Deletes the coin with the given id
-deleteCoin: function(id) {
-    return axios.delete("/api/coins/" + id);
-},
+const deleteCoin = (id) =>
+    axios.delete("/api/coins/" + id);
+
 // Saves a coin to the database
-saveCoin: function(coinData) {
-    return axios.post("/api/coins", coinData);
-}
+const saveCoin = (coinData) =>
+    axios.post("/api/coins", coinData);
+
+
+
+
 
 
 
 // example api request for protected data (sends error i user isn't logged in)
+
 const getProtectedExample = () => axios.get("/api/protected");
 
 // example api request for public data
 const getPublicExample = () => axios.get("/api/unprotected");
 
-const API = { getProtectedExample, getPublicExample };
+const API = { getCoin, getCoins, deleteCoin, saveCoin, getProtectedExample, getPublicExample };
 
 export default API;
-// one default export per module. 
+// one default export per module.
