824d6292e84b17d4b16475dcdf5d6c49b13d8cc0
doh <theshawns0n@gmail.com>
2020-11-17 11:01:49 -0500

    route template ++

--- controllers/geckocontroller.js
+++ controllers/geckocontroller.js
@@ -25,14 +25,14 @@ module.exports = {
             result.volumeInfo.imageLinks.thumbnail
         )
       )
-      .then(apiBooks =>
-        db.Book.find().then(dbBooks =>
-          apiBooks.filter(apiBook =>
-            dbBooks.every(dbBook => dbBook.googleId.toString() !== apiBook.id)
+      .then(apiCoins =>
+        db.Coin.find().then(dbCoins =>
+          apiCoins.filter(apiCoin =>
+            dbCoins.every(dbCoin => dbCoin.googleId.toString() !== apiCoin.id)
           )
         )
       )
-      .then(books => res.json(books))
+      .then(coins => res.json(coins))
       .catch(err => res.status(422).json(err));
   }
 };
