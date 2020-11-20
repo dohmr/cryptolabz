e47bcd2bf2727aa073faa0917a96df61b9445951
Garret Bright <garret@drinknewtopia.com>
2020-11-20 11:27:13 -0800

    added fav icon, need to work on toggle

--- client/src/components/CoinForm.js
+++ client/src/components/CoinForm.js
@@ -1,11 +1,13 @@
 import React from "react";
 
+
+
 // pass in props for information to be added when mapped through for coin and portfolio pages
 
 function CoinForm() {
     return (
       <tr>
-      <td>Fav Icon</td>
+      <td><i class="far fa-star"></i></td>
       <td>Bitcoin</td>
       <td>16,000</td>
       <td>1.2%</td>
