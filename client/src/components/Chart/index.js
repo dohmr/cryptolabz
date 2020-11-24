348b580dfc1dd0eb59130e55c0f8a380a54b7a7c
doh <theshawns0n@gmail.com>
2020-11-24 11:17:12 -0500

    added placeholder data for chart. cleaned up code in portfolio

--- client/src/components/Chart/index.js
+++ client/src/components/Chart/index.js
@@ -5,7 +5,7 @@ const data = {
   labels: ['Initial', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7'],
   datasets: [
     {
-      label: 'Faux Investment',
+      label: 'Bitcoin Investment',
       fill: false,
       lineTension: 0.1,
       backgroundColor: 'rgba(75,192,192,0.4)',
@@ -23,8 +23,49 @@ const data = {
       pointHoverBorderWidth: 2,
       pointRadius: 1,
       pointHitRadius: 10,
-      // options={{ maintainAspectRatio: false }},
       data: [40, 59, 80, 81, 56, 55, 40]
+    },
+    {
+      label: 'Litecoin Investment',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(885,0,00,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(25,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [40, 30, 20, 61, 76, 75, 60]
+    },
+    {
+      label: 'Ethereum Investment',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(0,200,00,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(25,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [40, 50, 30, 61, 46, 95, 40]
     }
   ]
 };
--- client/src/components/ProtectedPortfolioPage.js
+++ client/src/components/ProtectedPortfolioPage.js
@@ -10,30 +10,13 @@ import CoinForm from "./CoinForm"
 
 
 function PortfolioPage(props) {
-  const [image, setImage] = useState("");
-  const [coin, setCoin] = useState("");
-  const [price, setPrice] = useState("");
-  const [day, setDay] = useState("");
-  const [week, setWeek] = useState("");
-  const [id, setId] = useState("");
   const [coins, setCoins] = useState([]);
-  const [favcoins, setFavcoins] = useState([]);
-
-
 
   useEffect(() => {
     API.getFavcoins(true)
       .then((res) => {
         console.log(res.data)
         setCoins(res.data);
-
-        setId(res.data[0].id);
-        setImage(res.data[0].image);
-        setCoin(res.data[0].name);
-        setPrice(res.data[0].current_price);
-        setDay(res.data[0].price_change_percentage_24h);
-        setWeek(res.data[0].ath_change_percentage);
-
 
       })
     console.log(coins)
